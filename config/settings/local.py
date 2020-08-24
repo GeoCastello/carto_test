632e29d1125260734b6257cccd004cb9e9c7f392
GeoCastello <adrianupvtopo@gmail.com>
2020-08-24 18:24:37 +0200

    testing push problem with django templates

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,22 +13,6 @@ ALLOWED_HOSTS = [
     "*"
 ]
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(ROOT_DIR, "templates")],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
 # django-extensions
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
